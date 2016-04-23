b3a9a0c0279d2a585016d73eaf633f4817ce0abf
ajpereira13 <a.j.pereira13@hotmail.com>
2016-04-22 20:12:28 -0400

    Commit

---
+++ src/edu/exeter/cs/Account.java
@@ -0,0 +1,34 @@
+package edu.exeter.cs;
+
+public class Account {
+	
+	private double balance;
+	
+	public Account(){
+		balance = 0;
+	}
+	
+	public void setAcct(double num){
+		balance = num;
+	}
+	
+	public void deposit(double num){
+		balance = balance + num;
+	}
+	
+	public void withdraw(double num){
+		balance = balance - num;
+	}
+	
+	public double getBalance(){
+		return balance;
+	}
+	
+	public int setAcctNum(){
+		
+	}
+	
+	public int getAcctNum(){
+		
+	}
+}
